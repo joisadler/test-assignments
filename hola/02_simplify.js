2785ed7a646de9cd591ebdb0b13f49ca9a065d66
Yossi Adler <joisadler@gmail.com>
2017-10-26 10:46:05 +0300

    add hola simplify working solution

--- hola/02_simplify.js
+++ hola/02_simplify.js
@@ -1,7 +1,6 @@
 /* original code
 --------------------------------------------------------------------------------
 */
-
 // function func(s, a, b)
 // {
 //    var match_empty=/^$/ ;
@@ -40,47 +39,12 @@
 //        }
 //    }
 // }
+
+
 /* my solution
 --------------------------------------------------------------------------------
 */
 
-function func(s, a, b)
-{
-   var match_empty=/^$/ ;
-   if (s.match(match_empty))
-   {
-       return -1;
-   }
-   else
-   {
-       var i=s.length-1;
-       var aIndex=-1;
-       var bIndex=-1;
-
-       while ((aIndex==-1) && (bIndex==-1) && (i>=0))
-       {
-           if (s.substring(i, i+1) == a)
-               aIndex=i;
-       	if (s.substring(i, i+1) == b)
-               bIndex=i;
-       	i--;
-       }
-
-       if (aIndex != -1)
-       {
-           if (bIndex == -1)
-               return aIndex;
-       	else
-               return Math.max(aIndex, bIndex);
-       }
-       else
-       {
-           if (bIndex != -1)
-               return bIndex;
-	     else
-               return -1;
-       }
-   }
-}
+const func = (s, a, b) => Math.max(s.lastIndexOf(a), s.lastIndexOf(b));
 
 export default func;
